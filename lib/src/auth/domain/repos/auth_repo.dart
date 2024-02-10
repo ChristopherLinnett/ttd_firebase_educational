3db4578addeb479125459634d1faa2b73c6bd5c4
Christopher Linnett <chrislinnett45389@gmail.com>
2024-02-10 10:44:39 +0700

    add Auth entities and AuthRepo abstraction

---
+++ lib/src/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,25 @@
+import 'package:ttd_firebase_educational/core/enums/update_user.dart';
+import 'package:ttd_firebase_educational/core/utils/type_defs.dart';
+import 'package:ttd_firebase_educational/src/auth/domain/entities/user.dart';
+
+abstract class AuthRepo {
+  const AuthRepo();
+
+  ResultFuture<LocalUser> signIn({
+    required String email,
+    required String password,
+  });
+
+  ResultFuture<void> signUp({
+    required String email,
+    required String fullName,
+    required String password,
+  });
+
+  ResultFuture<void> forgotPassword(String email);
+
+  ResultFuture<void> updateuser({
+    required UpdateUserAction action,
+    dynamic userData,
+  });
+}
